59f05b53b54f330ec70098a62bb05224d8ca8e4e
Hannan Arif <hannanarif96@gmail.com>
2025-09-11 04:11:00 +0800

    Added push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,15 @@ import 'package:batami/helpers/utils.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:no_screenshot/no_screenshot.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
 
 void main() async {
   await GetStorage.init();
@@ -19,15 +23,19 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // You may set the permission requests to "provisional" which allows the user to choose what type
-// of notifications they would like to receive once the user receives a notification.
-final notificationSettings = await FirebaseMessaging.instance.requestPermission(provisional: true);
+  const AndroidInitializationSettings androidInit =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initSettings =
+  InitializationSettings(android: androidInit);
+  await flutterLocalNotificationsPlugin.initialize(initSettings);
 
-// For apple platforms, ensure the APNS token is available before making any FCM plugin API calls
-final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
-if (apnsToken != null) {
- // APNS token is available, make FCM plugin API requests...
-}
+  await FirebaseMessaging.instance.requestPermission(provisional: true);
+
+  // final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+  // if (apnsToken != null) {
+  //  // APNS token is available, make FCM plugin API requests...
+  // }
+
 
 FirebaseMessaging.instance.onTokenRefresh
     .listen((fcmToken) async {
@@ -36,6 +44,9 @@ FirebaseMessaging.instance.onTokenRefresh
     .onError((err) {
       // Error getting token.
     });
+
+
+  log("DeviceToken: ${await FirebaseMessaging.instance.getToken()}");
 
   FlutterError.onError = (details) {
     FlutterError.presentError(details);
@@ -55,6 +66,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      final notification = message.notification;
+      final android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'default_channel',
+              'General Notifications',
+              importance: Importance.high,
+              priority: Priority.high,
+            ),
+          ),
+        );
+      }
+    });
+
+
+
+
     return GetMaterialApp(
       localizationsDelegates: const [
         GlobalCupertinoLocalizations.delegate,
